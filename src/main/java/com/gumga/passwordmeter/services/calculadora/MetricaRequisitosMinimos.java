9a2f8aaa5b5a760b82b5adbec500a517eeaa9ea2
igorbraganca <igor.c.braganca@gmail.com>
2017-09-03 20:26:21 -0300

    commit

--- src/main/java/com/gumga/passwordmeter/services/calculadora/MetricaRequisitosMinimos.java
+++ src/main/java/com/gumga/passwordmeter/services/calculadora/MetricaRequisitosMinimos.java
@@ -5,14 +5,42 @@ import org.springframework.stereotype.Component;
 
 @Component
 @Qualifier("requesito-minimo")
-public class MetricaRequisitoMinimoTamanhoMaiorQueOito implements CalculadoraMetricas {
+public class MetricaRequisitosMinimos implements CalculadoraMetricas {
 
+	private final CalculadoraMetricas tamanho = new MetricaTamanho();
+	private final CalculadoraMetricas upperCase = new MetricaQuantidadeCaracteresCaixaAlta();
+	private final CalculadoraMetricas lowerCase = new MetricaQuantidadeCaracteresCaixaBaixa();
+	private final CalculadoraMetricas numbers = new MetricaQuantidadeAlgarismos();
+	private final CalculadoraMetricas symbols = new MetricaQuantidadeSimbolos();
+	
 	@Override
 	public int calcular(String senha) {
-		if(senha.length() > 8) {
-			return 1;
+		int soma = 0;
+		
+		if(upperCase.calcular(senha) > 0) {
+			soma++;
 		}
 		
-		return 0;
+		if(lowerCase.calcular(senha) > 0) {
+			soma++;
+		}
+		
+		if(numbers.calcular(senha) > 0) {
+			soma++;
+		}
+		
+		if(symbols.calcular(senha) > 0) {
+			soma++;
+		}
+		
+		if(soma < 3) {
+			return 0;
+		}
+		
+		if(tamanho.calcular(senha)/4 >= 8) {
+			return soma + 1;
+		} else {
+			return 0;
+		}
 	}
 }
--- src/test/java/com/gumga/passwordmeter/services/calculadora/MetricaRequisitosMinimosTeste.java
+++ src/test/java/com/gumga/passwordmeter/services/calculadora/MetricaRequisitosMinimosTeste.java
@@ -3,14 +3,14 @@ package com.gumga.passwordmeter.services.calculadora;
 import org.junit.Assert;
 import org.junit.Test;
 
-public class MetricaRequisitoMinimoTamanhoMairQueOitoTeste {
+public class MetricaRequisitosMinimosTeste {
 
 	@Test
-	public void deveRetornar1SeTamanhoMaiorQueOito() {
-		final CalculadoraMetricas m = new MetricaRequisitoMinimoTamanhoMaiorQueOito();
+	public void deveRetornar0SeRequisitoTamanhoNaoAlcancado() {
+		final CalculadoraMetricas m = new MetricaRequisitosMinimos();
 
-		String texto = "0123456789";
-		Assert.assertEquals(1, m.calcular(texto));
+		String texto = "aA1!";
+		Assert.assertEquals(0, m.calcular(texto));
 
 		texto = "";
 		Assert.assertEquals(0, m.calcular(texto));
@@ -18,13 +18,52 @@ public class MetricaRequisitoMinimoTamanhoMairQueOitoTeste {
 		texto = "012345";
 		Assert.assertEquals(0, m.calcular(texto));
 		
-		texto = "anvjhsekjhdd";
-		Assert.assertEquals(1, m.calcular(texto));
+		texto = "abc12A@";
+		Assert.assertEquals(0, m.calcular(texto));
 		
 		texto = "abcxg";
 		Assert.assertEquals(0, m.calcular(texto));
+	}
+	
+	@Test
+	public void deveRetornar0SeRequisitoTamanhoAlcancadoMenosTresTiposCaracteres() {
+		final CalculadoraMetricas m = new MetricaRequisitosMinimos();
+
+		String texto = "A13524335";
+		Assert.assertEquals(0, m.calcular(texto));
+
+		texto = "AAABBBB2222";
+		Assert.assertEquals(0, m.calcular(texto));
 		
-		texto = "NSDUFBncn";
-		Assert.assertEquals(1, m.calcular(texto));
+		texto = "!@#$%4444";
+		Assert.assertEquals(0, m.calcular(texto));
+	}
+	
+	@Test
+	public void deveRetornar45SeRequisitoTamanhoAlcancadoMaisTresTiposCaracteres() {
+		final CalculadoraMetricas m = new MetricaRequisitosMinimos();
+
+		String texto = "aA13524335";
+		Assert.assertEquals(4, m.calcular(texto));
+
+		texto = "aaaBBBB2222";
+		Assert.assertEquals(4, m.calcular(texto));
+		
+		texto = "!@#$%44ffa";
+		Assert.assertEquals(4, m.calcular(texto));
+	}
+	
+	@Test
+	public void deveRetornar5SeRequisitoTamanhoAlcancadoMaisQuatroTiposCaracteres() {
+		final CalculadoraMetricas m = new MetricaRequisitosMinimos();
+
+		String texto = "aA13524335@";
+		Assert.assertEquals(5, m.calcular(texto));
+
+		texto = "aaaBBBB2222@";
+		Assert.assertEquals(5, m.calcular(texto));
+		
+		texto = "!@#$%44ffaA";
+		Assert.assertEquals(5, m.calcular(texto));
 	}
 }
